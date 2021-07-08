12e1ff3070c38eabf2c2e853f177ef8e22d9ef2f
LoneHippie <lonehippie.music@gmail.com>
2021-07-08 15:30:38 +0300

    added type based styling

--- src/components/PokemonMini.js
+++ src/components/PokemonMini.js
@@ -31,6 +31,8 @@ const PokemonMini = (props) => {
 
     const [ openCard, setOpenCard ] = useState(false);
 
+    const primaryType = pokemon.types[0].type.name;
+
     function toggleDisplay() {
         setOpenCard(!openCard);
     };
@@ -97,14 +99,73 @@ const PokemonMini = (props) => {
         )
     };
 
+    //gets non contrasting text color based on type to match with type background color
+    const textColor = (type) => {
+        if (type === 'grass' || type === 'water' || type === 'poison' || type === 'fighting' || type === 'dragon' || type === 'dark' || type === 'ghost' || type === 'psychic') {
+            return 'rgba(220, 220, 220, 1)';
+        } else {
+            return 'rgba(47, 79, 79, 1)';
+        };
+    };
+
+    //gets gradient based on primary type
+    const typeBackground = (type) => {
+        switch(true) {
+            case type === 'normal':
+                return "linear-gradient(to top left, rgba(169, 169, 169, 0.95), rgba(169, 169, 169, 0.92))";
+            case type === 'fire':
+                return "linear-gradient(to top left, rgba(238, 129, 48, 0.95), rgba(238, 129, 48, 0.92))";
+            case type === 'water':
+                return "linear-gradient(to top left, rgba(99, 144, 240, 0.95), rgba(99, 144, 240, 0.92))";
+            case type === 'electric':
+                return "linear-gradient(to top left, rgba(247, 208, 44, 0.95), rgba(247, 208, 44, 0.92))";
+            case type === 'grass':
+                return "linear-gradient(to top left, rgba(122, 199, 76, 0.95), rgba(122, 199, 76, 0.92))";
+            case type === 'ice':
+                return "linear-gradient(to top left, rgba(150, 217, 214, 0.95), rgba(150, 217, 214, 0.92))";
+            case type === 'fighting':
+                return "linear-gradient(to top left, rgba(194, 46, 40, 0.95), rgba(194, 46, 40, 0.92))";
+            case type === 'poison':
+                return "linear-gradient(to top left, rgba(163, 62, 161, 0.95), rgba(163, 62, 161, 0.92))";
+            case type === 'ground':
+                return "linear-gradient(to top left, rgba(226, 191, 101, 0.95), rgba(226, 191, 101, 0.92))";
+            case type === 'flying':
+                return "linear-gradient(to top left, rgba(169, 143, 243, 0.95), rgba(169, 143, 243, 0.92))";
+            case type === 'psychic':
+                return "linear-gradient(to top left, rgba(249, 85, 135, 0.95), rgba(249, 85, 135, 0.92))";
+            case type === 'bug':
+                return "linear-gradient(to top left, rgba(166, 185, 26, 0.95), rgba(166, 185, 26, 0.92))";
+            case type === 'rock':
+                return "linear-gradient(to top left, rgba(182, 161, 54, 0.95), rgba(182, 161, 54, 0.92))";
+            case type === 'ghost':
+                return "linear-gradient(to top left, rgba(115, 87, 151, 0.95), rgba(115, 87, 151, 0.92))";
+            case type === 'dragon':
+                return "linear-gradient(to top left, rgba(111, 53, 252, 0.95), rgba(111, 53, 252, 0.92))";
+            case type === 'dark':
+                return "linear-gradient(to top left, rgba(112, 87, 70, 0.95), rgba(112, 87, 70, 0.92))";
+            case type === 'steel':
+                return "linear-gradient(to top left, rgba(183, 183, 206, 0.95), rgba(183, 183, 206, 0.92))";
+            case type === 'fairy':
+                return "linear-gradient(to top left, rgba(214, 133, 173, 0.95), rgba(214, 133, 173, 0.92))";
+            default:
+                return 'magenta'; //something ovbious in case of an error
+        }
+    };
+
+    //type based color styling for all cards
+    const typeStyles = {
+        text: textColor(primaryType),
+        background: typeBackground(primaryType)
+    };
+
     console.log(`Rendered pokemon ${pokemon.id}`);
 
     return (
         <>
             <section 
                 id={`card-${pokemon.id}`}
-                className={classes.card} 
-                // style={{background: eval(`styles.gradient_${typeName}`)}} 
+                className={classes.card}   
+                style={{background: typeStyles.background}}     
                 onClick={() => toggleDisplay()}
             >
 
@@ -114,7 +175,7 @@ const PokemonMini = (props) => {
                     </div>
                     <div 
                         className={classes.infoNumber} 
-                        // style={{color: textColor(typeName)}}
+                        style={{color: typeStyles.text}}
                     >
                         {`#${pokemon.id}`}
                     </div>
@@ -130,14 +191,9 @@ const PokemonMini = (props) => {
 
                 <div 
                     className={classes.name} 
-                    // style={{color: textColor(typeName)}}
+                    style={{color: typeStyles.text}}
                 >
                     {pokemon.name}
-                </div>
-
-                <div
-                    id={`pk-${pokemon.id}`}
-                    className="pokecard--mask">
                 </div>
 
             </section>
